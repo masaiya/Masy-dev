df78301a4390a1be3c9c827dd350d8b12d68ee2a
unknown <masy8511@firstshare.cn>
2020-09-29 11:51:24 +0800

    update

--- src/components/subGroup/mRadio.vue
+++ src/components/subGroup/mRadio.vue
@@ -1,15 +1,39 @@
 <template>
-  <div></div>
+  <label class="mRadio" :class="{'is-checked': model === label}"
+      role="radio" :aria-checked="model === label">
+    <span class="mRadio-input" :class="{'is-checked': model !== label}">
+      <span class="mRadio-inner"></span>
+      <input type="radio" ref="radio" class="mRadio-original" :value="label" v-model="model">
+      <span class="mRadio-label">
+        <slot></slot>
+        <template v-if="!$slots.default">{{label}}</template>
+      </span>
+    </span>
+  </label>
 </template>
 
 <script>
 export default {
   name: 'mRadio',
+  props: {
+    label: {}
+  },
   data() {
     return {
+      value: {}
     };
   },
   computed:{
+    model: {
+      get() {
+        console.log(this.value);
+        return this.value;
+      },
+      set(val) {
+        this.$emit('input', val);
+        this.$refs.radio && (this.$refs.radio.checked = this.model === this.label);
+      }
+    }
   },
   watch:{
   },
@@ -17,6 +41,61 @@ export default {
 };
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.mRadio {
+  font-size: 14px;
+  line-height: 1;
+  outline: 0;
+  white-space: nowrap;
+  color: #606266;
+  font-weight: 500;
+  cursor: pointer;
+  margin-right: 30px;
+  display: inline-block;
+  position: relative;
+  .mRadio-inner {
+    width: 14px;
+    height: 14px;
+    border: 1px solid #dcdfe6;
+    border-radius: 50%;
+    background-color: #fff;
+    display: inline-block;
+    position: relative;
+    line-height: 1;
+    vertical-align: middle;
+    &::after {
+      width: 5px;
+      height: 5px;
+      border-radius: 50%;
+      background-color: #fff;
+      content: "";
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
+  .mRadio-label {
+    line-height: 1;
+    vertical-align: middle;
+  }
+}
+.mRadio.is-checked {
+  border-color: #409eff;
+}
+.mRadio-input.is-checked .mRadio-inner {
+  border-color: #409EFF;
+  background: #409EFF
+}
+.mRadio-original {
+  opacity: 0;
+  outline: 0;
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  margin: 0;
+}
 </style>
--- src/docs/zh-CN/mdRadio.md
+++ src/docs/zh-CN/mdRadio.md
@@ -1,1 +1,25 @@
 # Radio 单选框
+在一组备选项中进行单选。
+
+### 基础用法
+由于选项默认可见，不宜过多，若选项过多，建议使用Select选择器
+
+
+:::demo 要使用 Radio 组件，只需要设置`v-model`绑定变量，选中意味着变量的值为相应 Radio `label`属性的值，`label`可以是`String`、`Number`或`Boolean`。
+```html
+<m-radio v-model="radio" label="1">备选项1</m-radio>
+<m-radio v-model="radio" label="2">备选项2</m-radio>
+<script>
+  import mRadio from "../../components/subGroup/mRadio.vue"
+  export default {
+    components: {
+      mRadio
+    },
+    data() {
+      return {
+        radio: '1'
+      }
+    }
+  };
+</script>
+```
