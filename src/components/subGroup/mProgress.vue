19d1a87dd2cdff62c0e834bffbb2d1257425f9a9
unknown <masy8511@firstshare.cn>
2020-10-13 19:30:03 +0800

    finish insideShow

--- src/components/subGroup/mProgress.vue
+++ src/components/subGroup/mProgress.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="mProgress" :class="['m-progress-'+type, status ? 'is'+status : '']">
+  <div class="mProgress" :class="['m-progress-'+type, status ? 'is-'+status : '']">
     <div class="m-progress-bar">
-      <div class="m-progress-outer">
-        <div class="m-progress-inner" :style="innerWidthStyle"></div>
+      <div class="m-progress-outer" :style="strokeHeight">
+        <div class="m-progress-inner" :style="innerWidthStyle">
+          <div class="m-progree-innerText" v-if="textInside">{{percentage+"%"}}</div>
+        </div>
       </div>
-      <div class="m-progress-text">{{percentage+"%"}}</div>
+      <div class="m-progress-text" v-if="!textInside">
+        <template v-if="!status">{{percentage+"%"}}</template>
+        <i v-else :class="iconClass"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -30,12 +35,33 @@ export default {
     status: {
       type: String,
       validator: val => ['success', 'exception', 'warning'].indexOf(val) > -1
+    },
+    height: {
+      type: Number,
+      default: 6
+    },
+    textInside: {
+      type: Boolean,
+      default: false
     }
   },
   computed:{
     innerWidthStyle() {
       const style = {};
       style.width = this.percentage + '%';
+      return style;
+    },
+    iconClass() {
+      if(this.status === 'warning') 
+        return 'm-icon-warning';
+      else if(this.status === 'success') 
+        return 'm-icon-circle-check';
+      else 
+        return 'm-icon-circle-close';
+    },
+    strokeHeight() {
+      const style = {};
+      style.height = this.height + 'px';
       return style;
     }
   },
@@ -52,9 +78,9 @@ export default {
   .m-progress-bar {
     width: 100%;
     box-sizing: border-box;
+    margin-bottom: 20px;
     .m-progress-outer {
       width: 100%;
-      height: 6px;
       border-radius: 100px;
       background-color: #ebeef5;
       .m-progress-inner {
@@ -64,23 +90,32 @@ export default {
         left: 0;
         top: 0;
         height: 100%;
-        background-color: #409eff;
         border-radius: 100px;
         line-height: 1;
         transition: width .6s ease;
         box-sizing: border-box;
+        .m-progree-innerText {
+          display: inline-block;
+          margin: 0 5px;
+          vertical-align: middle;
+          font-size: 12px;
+          color: #fff;
+          position: absolute;
+          right: 0;
+          top: 25%;
+        }
       }
     }
   }
   .m-progress-text {
+    width: 40px;
     font-size: 14px;
-    color: #606266;
+    color: #fff;
     display: inline-block;
-    margin-left: 10px;
     line-height: 1;
     float: right;
     position: relative;
-    right: -50px;
+    right: -40px;
     top: -10px;
     text-align: left;
   }
@@ -89,5 +124,32 @@ export default {
   margin-bottom: 15px;
   width: 350px;
   display: block;
+}
+.m-progress-inner {
+  background-color: #409eff;
+}
+.mProgress.is-success {
+  .m-progress-inner {
+    background-color: #67C23A;
+  }
+  .m-progress-text {
+    color: #67C23A;
+  }
+}
+.mProgress.is-warning {
+  .m-progress-inner {
+    background-color: #E6A23C;
+  }
+  .m-progress-text {
+    color: #E6A23C;
+  }
+}
+.mProgress.is-exception {
+  .m-progress-inner {
+    background-color: #F56C6C;
+  }
+  .m-progress-text {
+    color: #F56C6C;
+  }
 }
 </style>
--- src/docs/zh-CN/mdProgress.md
+++ src/docs/zh-CN/mdProgress.md
@@ -3,21 +3,26 @@
 
 ### 线形进度条
 :::demo Progress 组件设置 `percentage` 属性即可，表示进度条对应的百分比，必填，必须在 0-100。通过 `format` 属性来指定进度条文字内容。
-```html 
+```html
 <m-progress :percentage="50"></m-progress>
-<m-progress :percentage="70"></m-progress>
-<m-progress :percentage="100" :status="success"></m-progress>
-<m-progress :percentage="98" :status="exception"></m-progress>
-<m-progress :percentage="70" :status="warning"></m-progress>
-<m-progress :percentage="50"></m-progress>
-<script>
-export default {
-  data() {
-    return {
+<m-progress :percentage="100"></m-progress>
+<m-progress :percentage="100" status="success"></m-progress>
+<m-progress :percentage="98" status="exception"></m-progress>
+<m-progress :percentage="70" status="warning"></m-progress>
 
-    }
-  }
-}
-</script>
 ``` 
+:::
+
+### 百分比内显
+
+百分比不占用额外控件，适用于文件上传等场景。
+
+:::demo Progress 组件可通过 `height` 属性更改进度条的高度，并可通过 `text-inside` 属性来将进度条描述置于进度条内部。
+
+```html
+<m-progress :text-inside="true" :height="26" :percentage="70"></m-progress>
+<m-progress :text-inside="true" :height="24" :percentage="100" status="success"></m-progress>
+<m-progress :text-inside="true" :height="22" :percentage="80" status="warning"></m-progress>
+<m-progress :text-inside="true" :height="20" :percentage="50" status="exception"></m-progress>
+```
 :::
